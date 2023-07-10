98ca0d08372ce51c849723a05127eac25c656fc8
auntduda <auntduda@gmail.com>
2023-07-09 21:53:36 -0300

    adicionando o relatorio e finalmente acabando este CACETE

--- src/generate_prime.py
+++ src/generate_prime.py
@@ -2,7 +2,7 @@ import random
 from .miller_rabin import MillerRabin
 
 class GeneratePrime:
-    
+
     def __init__(self) -> None:
         self.rng = random.SystemRandom()
         self.miller_rabin = MillerRabin()
@@ -11,7 +11,7 @@ class GeneratePrime:
         # gerando um numero e vendo se esse numero eh primo a partir do teste de primalidade de Miller-Rabin
 
         while True:
-            # pega um numero aleatorio entre 2^1024-1 e 2^1024
+            # pega um numero aleatorio entre dois numeros muito grandes
             isprime = (self.rng.randrange(1 << 1024 - 1, 1 << 1024) << 1) + 1
 
             # checando se esse numero eh primo por Miller-Rabin
